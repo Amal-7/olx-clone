c7fd571cb6075b26a129d401c733455f8b9fb567
Amal <amalta199@gmail.com>
2023-05-30 21:10:06 +0530

    additional changes

--- src/App.js
+++ src/App.js
@@ -8,25 +8,7 @@ function App() {
     <div>
       <button onClick={async()=>{
         const prodCollection =collection(db,'products')
-        const setData = await setDoc(doc(prodCollection,'tDloHgEbryrspWL19Z2X'),{
-          name:'Apple',
-          price:100000,
-          type:'Mobile'
-        })
-        // const prodDltd = await deleteDoc(doc(prodCollection,'U1Gx9Xydm3kbexV4BU3Y'))
-        // console.log('productDelete');
-        // const prodAdd = await addDoc(prodCollection,{
-        //   name:'Poco',
-        //   price:25000,
-        //   type:'Mobile'
-        // })
-        // console.log(prodAdd.id)
-
-        // const productSnapShot = await getDocs(prodCollection)
-        // const prodtList = productSnapShot.forEach((obj)=>{ 
-        //   console.log(obj.data()) 
-        // }) 
-        // return prodtList; 
+       
       }}>click me</button>
     </div>
   );
